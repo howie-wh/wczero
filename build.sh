bd047ececda387447aee8e2875ba1b8a9e8fb1bd
Hui Wu <wuhui@shopee.com>
2022-04-02 18:34:13 +0800

    [mod] add build docker

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -eu
 
 build_server() {
   export GO111MODULE=on
-  go mod tidy
-  #go mod vendor
+  # go mod tidy
+  # go mod vendor
 
   go build -o bin/user-rpc services/user/rpc/user.go
   go build -o bin/user-api services/user/api/user.go
@@ -18,14 +18,12 @@ build_server() {
 }
 
 run_server() {
-  .bin/user-rpc -f services/user/rpc/etc/user.yaml
-  .bin/user-api -f services/user/api/etc/user-api.yaml
-  .bin/wallpaper-rpc -f services/wallpaper/rpc/etc/wallpaper.yaml
-  .bin/wallpaper-api -f services/wallpaper/api/etc/wallpaper-api.yaml
+  nohup ./bin/user-rpc -f services/user/rpc/etc/user.yaml &
+  nohup ./bin/user-api -f services/user/api/etc/user-api.yaml &
+  nohup ./bin/wallpaper-rpc -f services/wallpaper/rpc/etc/wallpaper.yaml &
+  nohup ./bin/wallpaper-api -f services/wallpaper/api/etc/wallpaper-api.yaml &
   echo "run_server successful"
 }
 
 build_server
 #run_server
-
-pasue
