265049d58053c03a0f2f10c300d41410bccb4cef
Hui Wu <wuhui@shopee.com>
2022-04-05 01:17:28 +0800

    [mod] modify wechat login

--- common/jwtx/jwt.go
+++ common/jwtx/jwt.go
@@ -11,3 +11,13 @@ func GetToken(secretKey string, iat, seconds, uid int64) (string, error) {
 	token.Claims = claims
 	return token.SignedString([]byte(secretKey))
 }
+
+func GetWeChatToken(secretKey string, iat, seconds int64, openid string) (string, error) {
+	claims := make(jwt.MapClaims)
+	claims["exp"] = iat + seconds
+	claims["iat"] = iat
+	claims["openid"] = openid
+	token := jwt.New(jwt.SigningMethodHS256)
+	token.Claims = claims
+	return token.SignedString([]byte(secretKey))
+}
--- services/user/rpc/internal/logic/wechatloginlogic.go
+++ services/user/rpc/internal/logic/wechatloginlogic.go
@@ -2,6 +2,11 @@ package logic
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 
 	"wczero/services/user/rpc/internal/svc"
 	"wczero/services/user/rpc/user"
@@ -24,9 +29,31 @@ func NewWeChatLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *WeCha
 }
 
 func (l *WeChatLoginLogic) WeChatLogin(in *user.WeChatLoginRequest) (*user.WeChatLoginResponse, error) {
+	params := url.Values{}
+	Url, err := url.Parse("https://api.weixin.qq.com/sns/jscode2session")
+	if err != nil {
+		return nil, err
+	}
+	params.Set("appid", in.Appid)
+	params.Set("secret", l.svcCtx.Config.Salt)
+	params.Set("js_code", in.Code)
+	params.Set("grant_type", "authorization_code")
 
-	return &user.WeChatLoginResponse{
-		AccessToken:  "xxxxxxxxxxxxxxxxx",
-		AccessExpire: 123456789,
-	}, nil
+	Url.RawQuery = params.Encode() // 如果参数中有中文参数,这个方法会进行URLEncode
+	urlPath := Url.String()
+
+	resp, err := http.Get(urlPath)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("url: %s, err: %s", urlPath, err)
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var authResp user.WeChatLoginResponse
+	err = json.Unmarshal(body, &authResp)
+	if err != nil {
+		return nil, fmt.Errorf("url: %s, err: %s", body, err)
+	}
+
+	return &authResp, nil
 }
