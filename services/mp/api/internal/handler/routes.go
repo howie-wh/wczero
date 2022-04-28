c39dcce70446bd86b02e6eca782499c590170c1d
Hui Wu <wuhui@shopee.com>
2022-04-28 20:03:22 +0800

    [mod] modify mp token field name

--- services/mp/api/internal/handler/routes.go
+++ services/mp/api/internal/handler/routes.go
@@ -13,7 +13,7 @@ func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		[]rest.Route{
 			{
-				Method:  http.MethodPost,
+				Method:  http.MethodGet,
 				Path:    "/api/v1/mp/basic_setup",
 				Handler: MPBasicSetupHandler(serverCtx),
 			},
