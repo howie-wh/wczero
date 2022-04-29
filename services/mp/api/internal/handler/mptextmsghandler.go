6716448cf8197366ee91435477cbae8269b22b5f
Hui Wu <wuhui@shopee.com>
2022-04-29 15:06:40 +0800

    [mod] add text msg reply interface

--- services/mp/api/internal/handler/mptextmsghandler.go
+++ services/mp/api/internal/handler/mptextmsghandler.go
@@ -2,11 +2,8 @@ package handler
 
 import (
 	"encoding/xml"
-	"github.com/zeromicro/go-zero/core/logx"
-	"github.com/zeromicro/go-zero/rest/httpx"
-	"io/ioutil"
 	"net/http"
-	"wczero/common/response"
+	"wczero/common/httpx"
 	"wczero/services/mp/api/internal/logic"
 	"wczero/services/mp/api/internal/svc"
 	"wczero/services/mp/api/internal/types"
@@ -26,31 +23,13 @@ func MPTextMsgHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.MPTextMsgRequest
 		if err := httpx.Parse(r, &req); err != nil {
-			logx.Errorf("http.Parse err: %v\n", err)
 			httpx.Error(w, err)
 			return
 		}
-
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			logx.Errorf("read body err: %v\n", err)
-			httpx.Error(w, err)
-			return
-		}
-		err = xml.Unmarshal(body, &req)
-		if err != nil {
-			logx.Errorf("body Unmarshal err: %v\n", err)
-			httpx.Error(w, err)
-			return
-		}
-
-		logx.Infof("req body: %v\n", string(body))
-		logx.Infof("[消息接收] - 收到消息, 消息类型为: %s, 消息内容为: %v\n", req.MsgType, req)
 
 		l := logic.NewMPTextMsgLogic(r.Context(), svcCtx)
 		resp, err := l.MPTextMsg(req)
 		if err != nil {
-			logx.Errorf("MPTextMsg err: %v\n", err)
 			httpx.Error(w, err)
 			return
 		}
@@ -62,7 +41,6 @@ func MPTextMsgHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 			MsgType:      resp.MsgType,
 			Content:      resp.Content,
 		}
-		logx.Infof("resp body: %v\n", msgResp)
-		response.OkXml(w, &msgResp)
+		httpx.OkXml(w, &msgResp)
 	}
 }
